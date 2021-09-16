9f5d62577e0b5194871fdb175f2af5f6443169df
정성원 <tyhg56@naver.com>
2021-09-16 18:00:05 +0900

    Feat : "SKillBox 내용 추가"
    map 자료구조 Skills 추가
    AddSkill 함수 추가 : 새로운 스킬 받아서 Skills에 저장
    Get 함수 추가 : Index번호에 해당 하는 스킬 반환

--- GameServer/SkillBox.cpp
+++ GameServer/SkillBox.cpp
@@ -1,2 +1,33 @@
 #include "pch.h"
 #include "SkillBox.h"
+
+CSkillBox::CSkillBox()
+{
+}
+
+CSkillBox::~CSkillBox()
+{
+    for (auto SkillInfoIterator : _Skills)
+    {
+        st_SkillInfo* Skilinfo = SkillInfoIterator.second;
+        delete Skilinfo;
+    }    
+}
+
+void CSkillBox::Init()
+{
+
+}
+
+void CSkillBox::AddSkill(st_SkillInfo SkillInfo)
+{
+    st_SkillInfo* NewSkillInfo = new st_SkillInfo();
+    *NewSkillInfo = SkillInfo;
+
+    _Skills.insert(pair<byte, st_SkillInfo*>(SkillInfo._SlotIndex, NewSkillInfo));     
+}
+
+st_SkillInfo* CSkillBox::Get(int8 SlotIndex)
+{
+    return nullptr;
+}
--- GameServer/SkillBox.h
+++ GameServer/SkillBox.h
@@ -1,5 +1,25 @@
 #pragma once
-class SkillBox
+#include"GameObjectInfo.h"
+
+class CSkill;
+
+class CSkillBox
 {
+private:
+	//------------------------------
+	// 스킬박스가 소유중인 스킬 목록
+	//------------------------------
+	map<byte, st_SkillInfo*> _Skills;
+public:
+
+	CSkillBox();
+	~CSkillBox();
+
+	void Init();
+	
+	// 스킬 추가
+	void AddSkill(st_SkillInfo SkillInfo);
+	// 스킬 얻기
+	st_SkillInfo* Get(int8 SlotIndex);
 };
 
