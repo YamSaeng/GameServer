9987c44114fb787ec3b10f6e0c59e4e8f7f11f26
정성원 <tyhg56@naver.com>
2021-10-21 21:07:56 +0900

    Fix : " Inventory에서 관리하는 대상을 수정 "
    Inventory
    - st_ItemInfo* -> CItem*로 수정

--- GameServer/Inventory.cpp
+++ GameServer/Inventory.cpp
@@ -11,91 +11,94 @@ CInventory::~CInventory()
 {
 	for (auto ItemIteraotr : _Items)
 	{
-		st_ItemInfo* Item = ItemIteraotr.second;
+		CItem* Item = ItemIteraotr.second;
 		delete Item;		
 	}
 }
 
 void CInventory::Init()
 {
-	// INVENTORY_SIZE 만큼 할당
-	for (int8 SlotIndex = 0; SlotIndex < (int8)en_Inventory::INVENTORY_SIZE; SlotIndex++)
-	{
-		// 빈껍데기 정보 생성
-		st_ItemInfo* InitItemInfo = new st_ItemInfo();		
-		InitItemInfo->SlotIndex = SlotIndex;
-
-		InitItemInfo->IsEquipped = false;
-		_Items.insert(pair<byte, st_ItemInfo*>(SlotIndex, InitItemInfo));
-	}
-
 	_BronzeCoinCount = 0;
 	_SliverCoinCount = 0;
 	_GoldCoinCount = 0;
 }
 
-void CInventory::AddItem(st_ItemInfo& ItemInfo)
-{
-	if (ItemInfo.ItemSmallCategory == en_SmallItemCategory::ITEM_SMALL_CATEGORY_NONE)
-	{
-		CRASH("Inventory Item is null");
-		return;
-	}
-
-	auto FindSlotIterator = _Items.find(ItemInfo.SlotIndex);
-	if (FindSlotIterator == _Items.end())
-	{
-		return;
-	}
-
-	*(*FindSlotIterator).second = ItemInfo;	
-}
-
-void CInventory::AddCoin(CItem* Item)
+void CInventory::AddItem(CItem* Item)
 {
 	// 동전과 은전 추가할 경우, 100개가 넘게 되면
 	// 다음 동전을 100개당 1개씩 비례하게 개수를 구하고 추가한다.
 	byte SliverCoinCount;
 	int64 GoldCoinCount;
+
 	switch (Item->_ItemInfo.ItemSmallCategory)
 	{
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_WEAPON_SWORD_WOOD:		
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_ARMOR_WEAR_WOOD:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_ARMOR_HAT_LEATHER:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_ARMOR_BOOT_LEATHER:		
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_POTION_HEAL_SMALL:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_SKILLBOOK_CHOHONE:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_LEATHER:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_SLIMEGEL:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_STONE:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_WOOD_LOG:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_WOOD_FLANK:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_YARN:
+		{
+			auto FindSlotIterator = _Items.find(Item->_ItemInfo.SlotIndex);
+			if (FindSlotIterator == _Items.end())
+			{
+				return;
+			}
+
+			(*FindSlotIterator).second = Item;
+		}
+		break;
 	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_BRONZE_COIN:
-		_BronzeCoinCount += (byte)(Item->_ItemInfo.ItemCount);
-		SliverCoinCount = (byte)(_BronzeCoinCount / 100);
+		{			
+			_BronzeCoinCount += (byte)(Item->_ItemInfo.ItemCount);
+			SliverCoinCount = (byte)(_BronzeCoinCount / 100);
 
-		if (SliverCoinCount > 0)
-		{
-			_BronzeCoinCount = (byte)(_BronzeCoinCount - (SliverCoinCount * 100));
-		}
-		else
-		{
-			_BronzeCoinCount += (byte)(SliverCoinCount * 100);
-		}
+			if (SliverCoinCount > 0)
+			{
+				_BronzeCoinCount = (byte)(_BronzeCoinCount - (SliverCoinCount * 100));
+			}
+			else
+			{
+				_BronzeCoinCount += (byte)(SliverCoinCount * 100);
+			}
 
-		_SliverCoinCount += SliverCoinCount;
+			_SliverCoinCount += SliverCoinCount;	
+		}	
 		break;
 	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_SLIVER_COIN:
-		_SliverCoinCount += (byte)(Item->_ItemInfo.ItemCount);
-		GoldCoinCount = (byte)(_SliverCoinCount / 100);
-
-		if (GoldCoinCount > 0)
 		{
-			_SliverCoinCount = (byte)(_SliverCoinCount - (GoldCoinCount * 100));
-		}
-		else
-		{
-			_SliverCoinCount += (byte)(GoldCoinCount * 100);
-		}
+			_SliverCoinCount += (byte)(Item->_ItemInfo.ItemCount);
+			GoldCoinCount = (byte)(_SliverCoinCount / 100);
 
-		_GoldCoinCount += GoldCoinCount;
+			if (GoldCoinCount > 0)
+			{
+				_SliverCoinCount = (byte)(_SliverCoinCount - (GoldCoinCount * 100));
+			}
+			else
+			{
+				_SliverCoinCount += (byte)(GoldCoinCount * 100);
+			}
+
+			_GoldCoinCount += GoldCoinCount;
+		}			
 		break;
 	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_GOLD_COIN:
-		_GoldCoinCount += Item->_ItemInfo.ItemCount;
-		break;
+		{
+			CMaterial* Coin = (CMaterial*)Item;
+
+			_GoldCoinCount += Item->_ItemInfo.ItemCount;
+		}		
+		break;							
 	}
 }
 
-st_ItemInfo* CInventory::Get(int8 SlotIndex)
+CItem* CInventory::Get(int8 SlotIndex)
 {
 	auto FindItemIterator = _Items.find(SlotIndex);
 	if (FindItemIterator == _Items.end())
@@ -108,30 +111,59 @@ st_ItemInfo* CInventory::Get(int8 SlotIndex)
 
 bool CInventory::IsExistItem(en_SmallItemCategory ItemType, int16& ItemEach, int8* SlotIndex)
 {
-	for (auto ItemIteraotr : _Items)
-	{
-		st_ItemInfo* Item = ItemIteraotr.second;
-
-		if (Item != nullptr && Item->ItemSmallCategory == ItemType)
+	switch (ItemType)
+	{	
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_WEAPON_SWORD_WOOD:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_ARMOR_WEAR_WOOD:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_ARMOR_HAT_LEATHER:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_ARMOR_BOOT_LEATHER:
 		{
-			Item->ItemCount += ItemEach;
-			*SlotIndex = ItemIteraotr.first;
-			return true;
+			// 무기와 방어구의 경우 중복이 안되므로 비어 있는 슬롯 인덱스를 반환한다.
+			GetEmptySlot(SlotIndex);
+		}	
+		break;
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_POTION_HEAL_SMALL:		
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_SKILLBOOK_CHOHONE:		
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_LEATHER:		
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_SLIMEGEL:			
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_STONE:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_WOOD_LOG:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_WOOD_FLANK:
+	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_MATERIAL_YARN:
+		{
+			// 재료의 경우 중복이 가능 하므로 
+			for (auto ItemIterator : _Items)
+			{
+				CItem* Item = ItemIterator.second;				
+
+				if (Item != nullptr && Item->_ItemInfo.ItemSmallCategory == ItemType)
+				{
+					CMaterial* Material = (CMaterial*)Item;
+
+					// 재료의 개수를 증가
+					Item->_ItemInfo.ItemCount += ItemEach;
+					// 작업한 슬롯의 인덱스를 반환
+					*SlotIndex = ItemIterator.first;
+					return true;
+				}
+			}
 		}
+		break;
 	}
 
 	return false;
 }
 
-vector<st_ItemInfo*> CInventory::Find(en_SmallItemCategory ItemType)
+vector<CItem*> CInventory::Find(en_SmallItemCategory ItemType)
 {
-	vector<st_ItemInfo*> FindItem;
+	// 인벤토리에서 ItemType과 같은 모든 아이템을 찾아서 반환한다.
+	vector<CItem*> FindItem;
 
 	for (auto ItemIterator : _Items)
 	{
-		st_ItemInfo* Item = ItemIterator.second;
+		CItem* Item = (CItem*)ItemIterator.second;
 
-		if (Item->ItemSmallCategory == ItemType)
+		if (Item != nullptr && Item->_ItemInfo.ItemSmallCategory == ItemType)
 		{
 			FindItem.push_back(Item);
 		}
@@ -142,31 +174,32 @@ vector<st_ItemInfo*> CInventory::Find(en_SmallItemCategory ItemType)
 
 bool CInventory::GetEmptySlot(int8* SlotIndex)
 {
-	for (auto ItemIteraotr : _Items)
+	// 무기와 방어구의 경우 중복이 안되므로 비어 있는 슬롯 인덱스를 반환한다.
+	for (auto ItemIterator : _Items)
 	{
-		st_ItemInfo* Item = ItemIteraotr.second;
+		CItem* Item = ItemIterator.second;
 
-		if (Item->ItemSmallCategory == en_SmallItemCategory::ITEM_SMALL_CATEGORY_NONE)
+		if (Item->_ItemInfo.ItemSmallCategory == en_SmallItemCategory::ITEM_SMALL_CATEGORY_NONE)
 		{
-			*SlotIndex = ItemIteraotr.first;
+			*SlotIndex = ItemIterator.first;
 			return true;
-		}		
-	}		
+		}
+	}
 
 	return false;
 }
 
-void CInventory::SwapItem(st_ItemInfo& SwapAItemInfo, st_ItemInfo& SwapBItemInfo)
+void CInventory::SwapItem(CItem* SwapAItem, CItem* SwapBItem)
 {
-	auto FindSwapAItem = _Items.find(SwapAItemInfo.SlotIndex);
+	auto FindSwapAItem = _Items.find(SwapAItem->_ItemInfo.SlotIndex);
 	if (FindSwapAItem != _Items.end())
-	{		
-		*(*FindSwapAItem).second = SwapAItemInfo;		
+	{
+		(*FindSwapAItem).second = SwapAItem;
 	}
 
-	auto FindSwapBItem = _Items.find(SwapBItemInfo.SlotIndex);
+	auto FindSwapBItem = _Items.find(SwapBItem->_ItemInfo.SlotIndex);
 	if (FindSwapBItem != _Items.end())
 	{
-		*(*FindSwapBItem).second = SwapBItemInfo;		
+		(*FindSwapBItem).second = SwapBItem;
 	}
 }
--- GameServer/Inventory.h
+++ GameServer/Inventory.h
@@ -3,17 +3,11 @@
 
 class CInventory
 {
-private:
-	//---------------------------------------------
-	// 인벤토리가 소유중인 아이템 ( 포인터로 관리 )
-	//   ( SlotIndex, 아이템정보 )
-	//---------------------------------------------
-	map<byte, st_ItemInfo*> _Items;			
 public:	
 
-	//-------------------
+	//---------------------
 	// 보유하고 있는 동전
-	//-------------------
+	//---------------------
 	int16 _BronzeCoinCount;
 	int16 _SliverCoinCount;
 	int64 _GoldCoinCount;
@@ -21,19 +15,19 @@ public:
 	CInventory();
 	~CInventory();
 	
+	//----------------------
+	// 인벤토리 초기화
+	//----------------------
 	void Init();
+		
 	//------------------------
-	// 인벤토리에 Item 추가
+	// 인벤토리에 아이템 추가
 	//------------------------
-	void AddItem(st_ItemInfo& ItemInfo);	
-	//------------------------
-	// 인벤토리에 Coin 추가
-	//------------------------
-	void AddCoin(CItem* Item);
+	void AddItem(CItem* Item);
 	//--------------------------
 	// 아이템 반환
 	//--------------------------
-	st_ItemInfo* Get(int8 SlotIndex);
+	CItem* Get(int8 SlotIndex);
 	
 	//-----------------------------------------------------------------------
 	// 인벤토리에 이미 아이템이 있는지 확인 ( 아이템 인벤토리에 넣을때 확인 )
@@ -43,12 +37,22 @@ public:
 	//--------------------------------------
 	// ItemType 받아서 아이템정보 반환
 	//--------------------------------------
-	vector<st_ItemInfo*> Find(en_SmallItemCategory ItemType);
+	vector<CItem*> Find(en_SmallItemCategory ItemType);
 
 	//----------------------------------
 	// 비어 있는 슬롯 반환
 	//----------------------------------
 	bool GetEmptySlot(int8* SlotIndex);
-	void SwapItem(st_ItemInfo& SwapAItemInfo, st_ItemInfo& SwapBItemInfo);
+
+	//--------------------------------------
+	// 인벤토리 아이템 스왑
+	//--------------------------------------
+	void SwapItem(CItem* SwapAItem, CItem* SwapBItem);
+private:
+	//---------------------------------------------
+	// 인벤토리가 소유중인 아이템 ( 포인터로 관리 )
+	//   ( SlotIndex, 아이템정보 )
+	//---------------------------------------------
+	map<byte, CItem*> _Items;
 };
 
