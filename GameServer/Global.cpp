6d88e445ef2be997332544918d82b9d42faea32f
정성원 <tyhg56@naver.com>
2021-11-19 01:04:46 +0900

    Fix : " 데이터 파싱 순서 변경 "
    Global
    - 스킬 데이터를 파싱하고 나서 캐릭터 능력치 데이터 파싱하도록 수정

--- GameServer/Global.cpp
+++ GameServer/Global.cpp
@@ -27,15 +27,17 @@ public:
 		G_Datamanager = new CDataManager();
 		// 아이템 데이터 파싱
 		G_Datamanager->LoadDataItem(L"ItemData.json");
-		// 플레이어 캐릭터 스테이터스 데이터 파싱
-		G_Datamanager->LoadDataPlayerCharacterStatus(L"PlayerCharacterStatus.json");
-		// 몬스터 데이터 파싱
-		G_Datamanager->LoadDataMonster(L"MonsterData.json");
 		// 스킬 데이터 파싱
 		G_Datamanager->LoadDataPublicSkill(L"PublicSkillDatas.json");
 		G_Datamanager->LoadDataWarriorSkill(L"WarriorSkillDatas.json");
 		G_Datamanager->LoadDataShamanSkill(L"ShamanSkillDatas.json");
 		G_Datamanager->LoadDataTaioistSkill(L"TaioistSkillDatas.json");
+		G_Datamanager->LoadDataThiefSkill(L"ThiefSkillDatas.json");
+		G_Datamanager->LoadDataArcherSkill(L"ArcherSkillDatas.json");
+		// 플레이어 캐릭터 스테이터스 데이터 파싱
+		G_Datamanager->LoadDataPlayerCharacterStatus(L"PlayerCharacterStatus.json");
+		// 몬스터 데이터 파싱
+		G_Datamanager->LoadDataMonster(L"MonsterData.json");		
 		// 환경 오브젝트 데이터 파싱
 		G_Datamanager->LoadDataEnvironment(L"EnvironmentData.json");
 		// 제작템 데이터 파싱
