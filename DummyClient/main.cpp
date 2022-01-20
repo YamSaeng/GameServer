68c5754ba4ebd9bedb0807c14a35731e465977ae
정성원 <tyhg56@naver.com>
2022-01-20 14:50:10 +0900

    Feat : " 더미 모니터링 변수 추가 "
    DummyClient
    - 모니터링 변수 추가 ( ConnectTPS, ConnectionTotal )

--- DummyClient/main.cpp
+++ DummyClient/main.cpp
@@ -26,11 +26,14 @@ int main()
 		wprintf(L"DummyClientStart Time [%04d-%02d-%02d %02d:%02d:%02d] \n", NowTime.wYear, NowTime.wMonth, NowTime.wDay, NowTime.wHour, NowTime.wMinute, NowTime.wSecond);
 		wprintf(L"================================================\n");
 		wprintf(L"Connect Client : [%ld]\n", G_DummyClient._ClientCount);
+		wprintf(L"Connect Total  : [%ld]\n", G_DummyClient._ConnectionTotal);
+		wprintf(L"ConnectTPS     : [%ld]\n", G_DummyClient._ConnectTPS);
 		wprintf(L"SendPacketTPS  : [%ld]\n", G_DummyClient._SendPacketTPS);
 		wprintf(L"RecvPacketTPS  : [%ld]\n", G_DummyClient._RecvPacketTPS);
 		wprintf(L"DisconnectTPS  : [%ld]\n", G_DummyClient._DisconnectTPS);
 		wprintf(L"================================================\n\n");
 		
+		G_DummyClient._ConnectTPS = 0;
 		G_DummyClient._SendPacketTPS = 0;
 		G_DummyClient._RecvPacketTPS = 0;
 		G_DummyClient._DisconnectTPS = 0;
