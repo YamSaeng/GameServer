77f0b476dfe7b807bce861d274c43b7dd8a8290f
정성원 <tyhg56@naver.com>
2022-03-07 15:50:31 +0900

    Feat : " 계정 테이블 수정, 로그인 테이블 추가 "
    LoginServer
    - LoginServerDB.xml
     - 계정 테이블 수정 ( 기본 키 ( AccountName ) 설정, 유니크 키 ( AccountDBId ) 설정 )
     - 로그인 테이블 추가
     - 계정 넣기, 계정 있는지 확인, 로그인 정보 넣기, 로그인 정보 있는지 확인 프로시저 추가

--- LoginServer/DBStoreProcedure.h
+++ LoginServer/DBStoreProcedure.h
@@ -3,20 +3,47 @@
 
 namespace SP
 {
+	// 계정 새로 생성
 	class CLoginServerDBAccountNew : public CDBBind<2, 0>
 	{
 	public:
-		CLoginServerDBAccountNew(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.}") {}
+		CLoginServerDBAccountNew(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spAccountNew(?,?)}") {}
 
 		void InAccountName(wstring& AccountName) { BindParam(0, AccountName.c_str()); }
 		void InPassword(wstring& Password) { BindParam(1, Password.c_str()); }
 	};
 
-	// DB에 입력한 해당 캐릭터가 있는지 확인
-	class CDBGameServerCharacterNameGet : public CDBBind<2, 0>
+	// 계정 테이블에 계정 있는지 확인
+	class CLoginServerAccountDBGetAccount : public CDBBind<2, 0>
 	{
 	public:
-		CDBGameServerCharacterNameGet(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spGetCharacterName(?)}") {}
-		void InCharacterName(wstring& CharacterName) { BindParam(0, CharacterName.c_str()); }
+		CLoginServerAccountDBGetAccount(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spGetAccount(?,?)}") {}
+		void InAccountName(wstring& AccountName) { BindParam(0, AccountName.c_str()); }
+		void InPassword(wstring& Password) { BindParam(1, Password.c_str()); }
 	};	
+
+	// 계정 로그인
+	class CLoginServerDBAccountLogin : public CDBBind<4, 0>
+	{
+	public:
+		CLoginServerDBAccountLogin(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spAccountLogin(?,?,?,?)}") {}
+		void InAccountID(int64& AccountID) { BindParam(0, AccountID); }		
+		void InAccountName(wstring& AccountName) { BindParam(1, AccountName.c_str()); }
+		void InPassword(wstring& Password) { BindParam(2, Password.c_str()); }		
+		void InLoginSuccessTime(TIMESTAMP_STRUCT& TokenExpiredTime) { BindParam(3, TokenExpiredTime); }
+	};
+
+	// 로그인 테이블에 계정 있는지 확인
+	class CLoginServerLoginDBGetAccount : public CDBBind<2, 0>
+	{
+	public:
+		CLoginServerLoginDBGetAccount(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spGetLogin(?,?)}") {}
+		void InAccountID(int64 AccountID) { BindParam(0, AccountID); }
+		void InAccountName(wstring& AccountName) { BindParam(1, AccountName.c_str()); }
+	};
+
+	class CTokenServerDBGetToken : public CDBBind<2, 0>
+	{
+		CTokenServerDBGetToken(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spGetAccount(?,?)}") {}
+	};
 }
