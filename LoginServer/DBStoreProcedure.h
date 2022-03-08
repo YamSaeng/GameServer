4df8b5ad2d1712880ff70dbe6d8ba013783a6a21
정성원 <tyhg56@naver.com>
2022-03-08 22:49:51 +0900

    Fix : " 로그인 서버 DB 수정 "
    LoginServer
    - LoginServerDB.xml
     - 계정 테이블
      - Password 타입 변경 문자열 -> 바이트 배열
     - 로그인 테이블
      - LoginState 컬럼 추가 ( 로그인 상태 ) 0 = 로그아웃 1 = 로그인
      - Password 타입 변경 문자열 -> 바이트 배열
     - 토큰 테이블 추가
     - 서버목록 테이블 추가
     - 로그인상태 업데이트, 토큰 정보 넣기, 토근 정보 확인, 토큰 업데이트 프로시저 추가

--- LoginServer/DBStoreProcedure.h
+++ LoginServer/DBStoreProcedure.h
@@ -10,40 +10,82 @@ namespace SP
 		CLoginServerDBAccountNew(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spAccountNew(?,?)}") {}
 
 		void InAccountName(wstring& AccountName) { BindParam(0, AccountName.c_str()); }
-		void InPassword(wstring& Password) { BindParam(1, Password.c_str()); }
+		template<int8 Length> void InPassword(BYTE(&Password)[Length]) { BindParam(1, Password); }		
 	};
 
 	// 계정 테이블에 계정 있는지 확인
-	class CLoginServerAccountDBGetAccount : public CDBBind<2, 0>
+	class CLoginServerAccountDBGetAccount : public CDBBind<1, 2>
 	{
 	public:
-		CLoginServerAccountDBGetAccount(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spGetAccount(?,?)}") {}
-		void InAccountName(wstring& AccountName) { BindParam(0, AccountName.c_str()); }
-		void InPassword(wstring& Password) { BindParam(1, Password.c_str()); }
+		CLoginServerAccountDBGetAccount(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spGetAccount(?)}") {}
+		void InAccountName(wstring& AccountName) { BindParam(0, AccountName.c_str()); }		
+
+		void OutAccountId(int64& AccountID) { BindCol(0, AccountID); }
+		template<int8 Length> void OutPassword(BYTE(&Password)[Length]) { BindCol(1, Password); }
 	};	
 
 	// 계정 로그인
-	class CLoginServerDBAccountLogin : public CDBBind<4, 0>
+	class CLoginServerDBAccountLogin : public CDBBind<5, 0>
 	{
 	public:
-		CLoginServerDBAccountLogin(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spAccountLogin(?,?,?,?)}") {}
-		void InAccountID(int64& AccountID) { BindParam(0, AccountID); }		
-		void InAccountName(wstring& AccountName) { BindParam(1, AccountName.c_str()); }
-		void InPassword(wstring& Password) { BindParam(2, Password.c_str()); }		
-		void InLoginSuccessTime(TIMESTAMP_STRUCT& TokenExpiredTime) { BindParam(3, TokenExpiredTime); }
+		CLoginServerDBAccountLogin(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spAccountLogin(?,?,?,?,?)}") {}
+		void InLoginState(int8& LoginState) { BindParam(0, LoginState); }
+		void InAccountID(int64& AccountID) { BindParam(1, AccountID); }		
+		void InAccountName(wstring& AccountName) { BindParam(2, AccountName.c_str()); }
+		template<int8 Length> void InPassword(BYTE(&Password)[Length]) { BindParam(3, Password); }		
+		void InLoginsuccessTime(TIMESTAMP_STRUCT& LoginSuccessTime) { BindParam(4, LoginSuccessTime); }
 	};
 
 	// 로그인 테이블에 계정 있는지 확인
-	class CLoginServerLoginDBGetAccount : public CDBBind<2, 0>
+	class CLoginServerLoginDBGetAccount : public CDBBind<2, 1>
 	{
 	public:
 		CLoginServerLoginDBGetAccount(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spGetLogin(?,?)}") {}
-		void InAccountID(int64 AccountID) { BindParam(0, AccountID); }
+		void InAccountID(int64& AccountID) { BindParam(0, AccountID); }
 		void InAccountName(wstring& AccountName) { BindParam(1, AccountName.c_str()); }
+
+		void OutLoginState(int8& LoginState) { BindCol(0, LoginState); }
 	};
 
-	class CTokenServerDBGetToken : public CDBBind<2, 0>
+	// 로그인 상태 업데이트
+	class CLoginServerLoginDBLoginStateUpdate : public CDBBind<2, 0>
 	{
-		CTokenServerDBGetToken(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spGetAccount(?,?)}") {}
+	public:
+		CLoginServerLoginDBLoginStateUpdate(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spLoginStateUpdate(?,?)}") {}
+		void InLoginState(int8& LoginState) { BindParam(0, LoginState); }
+		void InAccountID(int64& AccountID) { BindParam(1, AccountID); }
 	};
+
+	// 토큰 있는지 확인
+	class CTokenServerDBIsToken : public CDBBind<1, 2>
+	{
+	public:
+		CTokenServerDBIsToken(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spIsToken(?)}") {}
+		void InAccountID(int64& AccountID) { BindParam(0, AccountID); }
+
+		void OutTokenTime(TIMESTAMP_STRUCT& TokenTime) { BindCol(0, TokenTime); }
+		template<int8 Length> void OutToken(BYTE(&Token)[Length]) { BindCol(1, Token); }
+	};
+
+	// 새로운 토큰 생성해서 넣기
+	class CLoginServerDBTokenNew : public CDBBind<4, 0>
+	{
+	public:
+		CLoginServerDBTokenNew(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spTokenInfoInput(?,?,?,?)}") {}
+		void InAccountID(int64& AccountID) { BindParam(0, AccountID); }
+		template<int8 Length> void InToken(BYTE(&Token)[Length]) { BindParam(1, Token); }
+		void InLoginsuccessTime(TIMESTAMP_STRUCT& LoginSuccessTime) { BindParam(2, LoginSuccessTime); }
+		void InTokenExpiredTime(TIMESTAMP_STRUCT& TokenExpiredTime) { BindParam(3, TokenExpiredTime); }
+	};
+
+	// 토큰 새로 갱신하기
+	class CLoginServerDBTokenUpdate : public CDBBind<4, 0>
+	{
+	public:
+		CLoginServerDBTokenUpdate(CDBConnection& DBConnection) : CDBBind(DBConnection, L"{CALL dbo.spTokenUpdate(?,?,?,?)}") {}
+		void InAccountID(int64& AccountID) { BindParam(0, AccountID); }
+		template<int8 Length> void InToken(BYTE(&Token)[Length]) { BindParam(1, Token); }
+		void InLoginsuccessTime(TIMESTAMP_STRUCT& LoginSuccessTime) { BindParam(2, LoginSuccessTime); }
+		void InTokenExpiredTime(TIMESTAMP_STRUCT& TokenExpiredTime) { BindParam(3, TokenExpiredTime); }
+	};	
 }
