81a8fc19f6c4341204a274a8b84f67f1b5ab95ac
정성원 <tyhg56@naver.com>
2022-06-17 16:38:17 +0900

    Fix : " 용광로 수정 "
    GameServer
     - Furnace 수정
      - InputMaterialItem() 삭제 ( 제작대로 옮김 )
      - FindMaterialItem() 삭제 ( 제작대로 옮김 )

--- GameServer/Furnace.cpp
+++ GameServer/Furnace.cpp
@@ -1,6 +1,5 @@
 #include "pch.h"
 #include "Furnace.h"
-#include "Item.h"
 #include "ObjectManager.h"
 #include "DataManager.h"
 #include <atlbase.h>
@@ -14,63 +13,6 @@ CFurnace::CFurnace()
 
 	auto FindFurnaceData = G_Datamanager->_CraftingTableData.find((int16)_GameObjectInfo.ObjectType);
 	_CraftingTableRecipe = *(*FindFurnaceData).second;	
-}
-
-void CFurnace::InputMaterialItem(CItem* MaterialItem, int16 MaterialItemCount)
-{
-	// 우선 용광로 재료 템들 중에 넣을 아이템이 존재하는지 확인
-	auto FindMaterialIter = _MaterialItems.find(MaterialItem->_ItemInfo.ItemSmallCategory);
-	// 발견하지 못햇을 경우
-	if (FindMaterialIter == _MaterialItems.end())
-	{		
-		// 아이템 생성
-		CItem* NewMaterialItem = G_ObjectManager->ItemCreate(MaterialItem->_ItemInfo.ItemSmallCategory);	
-		if (NewMaterialItem != nullptr)
-		{
-			NewMaterialItem->_ItemInfo.ItemDBId = 0;
-			NewMaterialItem->_ItemInfo.InventoryItemNumber = 0;
-			NewMaterialItem->_ItemInfo.ItemIsQuickSlotUse = false;
-			NewMaterialItem->_ItemInfo.Rotated = false;
-			NewMaterialItem->_ItemInfo.Width = 1;
-			NewMaterialItem->_ItemInfo.Height = 1;
-			NewMaterialItem->_ItemInfo.TileGridPositionX = MaterialItem->_ItemInfo.TileGridPositionX;
-			NewMaterialItem->_ItemInfo.TileGridPositionY = MaterialItem->_ItemInfo.TileGridPositionY;
-			NewMaterialItem->_ItemInfo.ItemLargeCategory = MaterialItem->_ItemInfo.ItemLargeCategory;
-			NewMaterialItem->_ItemInfo.ItemMediumCategory = MaterialItem->_ItemInfo.ItemMediumCategory;
-			NewMaterialItem->_ItemInfo.ItemSmallCategory = MaterialItem->_ItemInfo.ItemSmallCategory;
-			NewMaterialItem->_ItemInfo.ItemName = MaterialItem->_ItemInfo.ItemName;
-			NewMaterialItem->_ItemInfo.ItemExplain = MaterialItem->_ItemInfo.ItemExplain;
-			NewMaterialItem->_ItemInfo.ItemMaxCount = MaterialItem->_ItemInfo.ItemMaxCount;
-			NewMaterialItem->_ItemInfo.ItemCount = MaterialItemCount;
-			NewMaterialItem->_ItemInfo.ItemThumbnailImagePath = MaterialItem->_ItemInfo.ItemThumbnailImagePath;
-			NewMaterialItem->_ItemInfo.ItemIsEquipped = MaterialItem->_ItemInfo.ItemIsEquipped;
-
-			_MaterialItems.insert(pair<en_SmallItemCategory, CItem*>(NewMaterialItem->_ItemInfo.ItemSmallCategory, NewMaterialItem));
-		}		
-	}	
-	else
-	{
-		// 발견 했을 경우 개수만 늘려줌
-		(*FindMaterialIter).second->_ItemInfo.ItemCount += MaterialItemCount;
-	}
-}
-
-bool CFurnace::FindMaterialItem(en_SmallItemCategory FindSmallItemCategory, int16 ItemCount)
-{
-	auto FindMaterialIter = _MaterialItems.find(FindSmallItemCategory);
-	if (FindMaterialIter == _MaterialItems.end())
-	{
-		return false;
-	}
-	
-	if ((*FindMaterialIter).second->_ItemInfo.ItemCount >= ItemCount)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
 }
 
 void CFurnace::Update()
--- GameServer/Furnace.h
+++ GameServer/Furnace.h
@@ -8,11 +8,6 @@ class CFurnace : public CCraftingTable
 public:
 	CFurnace();
 
-	// 용광로에 재료 아이템 넣기
-	void InputMaterialItem(CItem* MaterialItem, int16 MaterialItemCount);	
-	// 용광로에 아이템이 개수 만큼 있는지 확인
-	bool FindMaterialItem(en_SmallItemCategory FindSmallItemCategory, int16 ItemCount);
-
 	virtual void Update() override;
 	
 protected:
