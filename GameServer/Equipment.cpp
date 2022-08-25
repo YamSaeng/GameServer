373af24d43a144a34c88ba6b652016b7ea4eb026
정성원 <tyhg56@naver.com>
2022-08-25 10:51:04 +0900

    Fix : " Equipment 수정 "
    GameServer
     - Euqipment 수정
      - en_Equipment 추가
       - EQUIPMENT_PARTS_MAX 추가 ( 관리 중인 아이템 최대 개수 )
      - _EquipmentParts 추가 ( 관리 중인 장비 아이템 목록 )
      - ItemEquip() 수정

--- GameServer/Equipment.cpp
+++ GameServer/Equipment.cpp
@@ -11,16 +11,11 @@ CEquipment::CEquipment()
 	_WearArmorDefence = 0;
 	_BootArmorDefence = 0;
 
-	_HeadArmorItem = nullptr;
-	_WearArmorItem = nullptr;
-	_GloveArmorItem = nullptr;
-	_BootArmorItem = nullptr;
-	_WeaponItem = nullptr;
 }
 
 CEquipment::~CEquipment()
 {
-	
+
 }
 
 void CEquipment::ItemEquip(CItem* EquipItem, CPlayer* ReqEquipItemPlayer)
@@ -28,78 +23,34 @@ void CEquipment::ItemEquip(CItem* EquipItem, CPlayer* ReqEquipItemPlayer)
 	switch (EquipItem->_ItemInfo.ItemSmallCategory)
 	{
 	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_WEAPON_SWORD_WOOD:
-		{
-			_WeaponItem = EquipItem;
+		_EquipmentParts[en_EquipmentParts::EQUIPMENT_PARTS_LEFT_HAND]->_ItemInfo.ItemIsEquipped = false;
+		_EquipmentParts[en_EquipmentParts::EQUIPMENT_PARTS_LEFT_HAND] = EquipItem;
 
-			_WeaponMinDamage = _WeaponItem->_ItemInfo.ItemMinDamage;
-			_WeaponMaxDamage = _WeaponItem->_ItemInfo.ItemMaxDamage;
-
-			// 요청한 장비를 제외한 무기 중에서 착용 중인 장비 해제 
-			vector<CItem*> Weapons = ReqEquipItemPlayer->_Inventory.Find(EquipItem->_ItemInfo.ItemLargeCategory);
-			for (CItem* WeaponItem : Weapons)
-			{
-				if (WeaponItem->_ItemInfo.ItemIsEquipped == true)
-				{
-					WeaponItem->_ItemInfo.ItemIsEquipped = false;
-					break;
-				}
-			}			
-		}
-		break;		
+		//_WeaponMinDamage = _WeaponItem->_ItemInfo.ItemMinDamage;
+		//_WeaponMaxDamage = _WeaponItem->_ItemInfo.ItemMaxDamage;			
+		break;
+	case en_SmallItemCategory::ITEM_SAMLL_CATEGORY_WEAPON_WOOD_SHIELD:
+		_EquipmentParts[en_EquipmentParts::EQUIPMENT_PARTS_RIGHT_HAND]->_ItemInfo.ItemIsEquipped = false;
+		_EquipmentParts[en_EquipmentParts::EQUIPMENT_PARTS_RIGHT_HAND] = EquipItem;
+		break;
 	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_ARMOR_HAT_LEATHER:
-		{
-			_HeadArmorItem = EquipItem;
+		_EquipmentParts[en_EquipmentParts::EQUIPMENT_PARTS_HEAD]->_ItemInfo.ItemIsEquipped = false;
+		_EquipmentParts[en_EquipmentParts::EQUIPMENT_PARTS_HEAD] = EquipItem;
 
-			_HeadArmorDefence = _HeadArmorItem->_ItemInfo.ItemDefence;
-
-			// 요청한 장비를 제외한 머리 방어구 중에서 착용 중인 장비를 해제
-			vector<CItem*> HeadArmors = ReqEquipItemPlayer->_Inventory.Find(EquipItem->_ItemInfo.ItemLargeCategory);
-			for (CItem* HeadArmor : HeadArmors)
-			{
-				if (HeadArmor->_ItemInfo.ItemIsEquipped == true)
-				{
-					HeadArmor->_ItemInfo.ItemIsEquipped = false;
-					break;
-				}
-			}
-		}		
+		//_HeadArmorDefence = _HeadArmorItem->_ItemInfo.ItemDefence;							
 		break;
 	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_ARMOR_WEAR_WOOD:
-		{
-			_WearArmorItem = EquipItem;
+		_EquipmentParts[en_EquipmentParts::EQUIPMENT_PARTS_BODY]->_ItemInfo.ItemIsEquipped = false;
+		_EquipmentParts[en_EquipmentParts::EQUIPMENT_PARTS_BODY] = EquipItem;
 
-			_WearArmorDefence = _WearArmorItem->_ItemInfo.ItemDefence;
-
-			// 요청한 장비를 제외한 갑옷 방어구 중에서 착용 중인 장비를 해제
-			vector<CItem*> WearArmors = ReqEquipItemPlayer->_Inventory.Find(EquipItem->_ItemInfo.ItemLargeCategory);
-			for (CItem* WearArmor : WearArmors)
-			{
-				if (WearArmor->_ItemInfo.ItemIsEquipped == true)
-				{
-					WearArmor->_ItemInfo.ItemIsEquipped = false;
-					break;
-				}
-			}
-		}		
+		//_WearArmorDefence = _WearArmorItem->_ItemInfo.ItemDefence;							
 		break;
 	case en_SmallItemCategory::ITEM_SMALL_CATEGORY_ARMOR_BOOT_LEATHER:
-		{
-			_BootArmorItem = EquipItem;
+		_EquipmentParts[en_EquipmentParts::EQUIPMENT_PARTS_BOOT]->_ItemInfo.ItemIsEquipped = false;		
+		_EquipmentParts[en_EquipmentParts::EQUIPMENT_PARTS_BOOT] = EquipItem;
 
-			_BootArmorDefence = _BootArmorItem->_ItemInfo.ItemDefence;
-
-			// 요청한 장비를 제외한 신발 방어구 중에서 착용 중인 장비를 해제
-			vector<CItem*> BootArmors = ReqEquipItemPlayer->_Inventory.Find(EquipItem->_ItemInfo.ItemLargeCategory);
-			for (CItem* BootArmor : BootArmors)
-			{
-				if (BootArmor->_ItemInfo.ItemIsEquipped == true)
-				{
-					BootArmor->_ItemInfo.ItemIsEquipped = false;
-					break;
-				}
-			}
-		}		
-		break;	
+		//_BootArmorDefence = _BootArmorItem->_ItemInfo.ItemDefence;							
+		break;
 	}
 
 	EquipItem->_ItemInfo.ItemIsEquipped = true;
--- GameServer/Equipment.h
+++ GameServer/Equipment.h
@@ -17,20 +17,12 @@ public:
 
 	void ItemEquip(CItem* EquipItem, CPlayer* ReqEquipItemPlayer);
 private:
-	//----------------------------
-	// 장비 창이 관리중인 아이템
-	//----------------------------
-	
-	// 방어구
-	// 머리
-	CItem* _HeadArmorItem;
-	// 갑옷
-	CItem* _WearArmorItem;
-	// 장갑
-	CItem* _GloveArmorItem;
-	// 신발
-	CItem* _BootArmorItem;
-	// 무기
-	CItem* _WeaponItem;
+	enum en_Equipment
+	{
+		EQUIPMENT_PARTS_MAX = 6
+	};
+
+	// 관리중인 장비 아이템
+	CItem* _EquipmentParts[en_Equipment::EQUIPMENT_PARTS_MAX];	
 };
 
