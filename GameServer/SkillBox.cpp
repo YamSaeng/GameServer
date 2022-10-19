fdf2dca214fbb2edf1fa5628588f5d3594eb2a2b
정성원 <tyhg56@naver.com>
2022-10-19 11:50:35 +0900

    Fix : " SkillBox 수정 "
    GameServer
     - SkillBox 수정
      - _AttackSkills, _TacTicSkills, _BufSkills 삭제
      - _AttackSkills, _TacTicSkills, _BufSkills와 관련된 함수 삭제
      - en_SkillCharacteristicCount 추가
       - SKILL_CHARACTERISTIC_MAX_COUNT ( 스킬 특성 최대 개수 )
       - _SkillCharacteristicPublic ( 공용 스킬 특성 추가 )
       - _SkillCharcateristics ( 스킬 특성 배열 ( 최대 3개 ) )
       - FindSkill() 수정
        - CharacteristicType을 추가로 받아서 보유중인 스킬 특성과 비교후 스킬 찾기로 변경
       - CreateChracteristic() 추가 ( 스킬 특성에 맞게 Characteristic 생성 )
       - FindCharacteristic() 추가 ( 스킬 특성 찾기 )

--- GameServer/SkillBox.cpp
+++ GameServer/SkillBox.cpp
@@ -5,24 +5,17 @@
 
 CSkillBox::CSkillBox()
 {
+	_SkillCharacteristicPublic.SkillCharacteristicInit(en_SkillCharacteristic::SKILL_CATEGORY_PUBLIC);
+
+	for (int8 i = 0; i < (int8)en_SkillCharacteristicCount::SKILL_CHARACTERISTIC_MAX_COUNT; i++)
+	{
+		_SkillCharacteristics[i]._SkillBoxIndex = i;
+	}
 }
 
 CSkillBox::~CSkillBox()
 {
-	for (auto AttackSkillInfoIterator : _AttackSkills)
-	{
-		delete AttackSkillInfoIterator;
-	}	
-
-	for (auto TacTicSkillInfoIterator : _TacTicSkills)
-	{
-		delete TacTicSkillInfoIterator;
-	}
-
-	for (auto BufSkillInfoIterator : _BufSkills)
-	{
-		delete BufSkillInfoIterator;
-	}
+	
 }
 
 void CSkillBox::Init()
@@ -30,122 +23,72 @@ void CSkillBox::Init()
 
 }
 
-void CSkillBox::AddAttackSkill(CSkill* AttackSkillInfo)
+CSkillCharacteristic* CSkillBox::FindCharacteristic(int8 FindCharacteristicIndex, int8 FindCharacteristicType)
 {
-	_AttackSkills.push_back(AttackSkillInfo);
+	if (_SkillCharacteristics[FindCharacteristicIndex]._SkillCharacteristic == (en_SkillCharacteristic)FindCharacteristicType)
+	{
+		return &_SkillCharacteristics[FindCharacteristicIndex];
+	}
+	else
+	{
+		return nullptr;
+	}	
 }
 
-void CSkillBox::AddTacTicSkill(CSkill* TacTicSkillInfo)
+void CSkillBox::CreateChracteristic(int8 ChracteristicIndex, int8 CharacteristicType)
 {
-	_TacTicSkills.push_back(TacTicSkillInfo);
+	if (ChracteristicIndex > 2)
+	{
+		CRASH("Overflow")
+	}
+
+	_SkillCharacteristics[ChracteristicIndex].SkillCharacteristicInit((en_SkillCharacteristic)CharacteristicType);
 }
 
-void CSkillBox::AddBufSkill(CSkill* BufSkillInfo)
-{
-	_BufSkills.push_back(BufSkillInfo);
-}
-
-CSkill* CSkillBox::FindSkill(en_SkillType SkillType)
+CSkill* CSkillBox::FindSkill(en_SkillCharacteristic CharacteristicType, en_SkillType SkillType)
 {
 	CSkill* FindSkill = nullptr;
 
-	// 스킬 찾기
-	switch ((en_SkillType)SkillType)
-	{
-		// 기본 공격
-	case en_SkillType::SKILL_DEFAULT_ATTACK:
-		// 전사 공격
-	case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_FIERCE_ATTACK:
-	case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_CONVERSION_ATTACK:
-	case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_SMASH_WAVE:
-	case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_SHAHONE:
-	case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_CHOHONE:
-	case en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SHIELD_SMASH:
-		// 주술사 공격
-	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_FLAME_HARPOON:
-	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ROOT:
-	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ICE_CHAIN:
-	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ICE_WAVE:
-	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_LIGHTNING_STRIKE:
-	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_HEL_FIRE:
-		// 도사 공격
-	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_DIVINE_STRIKE:
-	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_ROOT:
-		FindSkill = FindAttackSkill((en_SkillType)SkillType);
+	switch (CharacteristicType)
+	{	
+	case en_SkillCharacteristic::SKILL_CATEGORY_PUBLIC:
+		FindSkill = _SkillCharacteristicPublic.FindSkill(SkillType);
 		break;
-		// 주술사 전술
-	case en_SkillType::SKILL_SPELL_ACTIVE_BUF_TELEPORT:
-		// 도사 전술
-	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_HEALING_LIGHT:
-	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_HEALING_WIND:
-		FindSkill = FindTacTicSkill((en_SkillType)SkillType);
-		break;
-		// 전사 버프
-	case en_SkillType::SKILL_FIGHT_ACTIVE_BUF_CHARGE_POSE:
-	case en_SkillType::SKILL_PUBLIC_ACTIVE_BUF_SHOCK_RELEASE:
-		FindSkill = FindBufSkill((en_SkillType)SkillType);
-		break;
-	}
+	case en_SkillCharacteristic::SKILL_CATEGORY_FIGHT:		
+	case en_SkillCharacteristic::SKILL_CATEGORY_PROTECTION:		
+	case en_SkillCharacteristic::SKILL_CATEGORY_SPELL:		
+	case en_SkillCharacteristic::SKILL_CATEGORY_SHOOTING:		
+	case en_SkillCharacteristic::SKILL_CATEGORY_DISCIPLINE:		
+	case en_SkillCharacteristic::SKILL_CATEGORY_ASSASSINATION:
+		{
+			for (int8 i = 0; i < (int8)en_SkillCharacteristicCount::SKILL_CHARACTERISTIC_MAX_COUNT; i++)
+			{
+				FindSkill = _SkillCharacteristics[i].FindSkill(SkillType);
+			}
+		}
+		break;	
+	}	
 
 	return FindSkill;
 }
 
 void CSkillBox::Update()
 {
-	for (CSkill* AttackSkill : _AttackSkills)
+	for (int8 i = 0; i < (int8)en_SkillCharacteristicCount::SKILL_CHARACTERISTIC_MAX_COUNT; i++)
 	{
-		AttackSkill->Update();
-	}
-
-	for (CSkill* TacTicSkill : _TacTicSkills)
-	{
-		TacTicSkill->Update();
-	}
-
-	for (CSkill* BufSkill : _BufSkills)
-	{
-		BufSkill->Update();
-	}
+		if (_SkillCharacteristics[i]._SkillCharacteristic != en_SkillCharacteristic::SKILL_CATEGORY_NONE)
+		{
+			_SkillCharacteristics[i].CharacteristicUpdate();
+		}
+	}	
 }
 
 void CSkillBox::Empty()
 {
-	for (CSkill* AttackSkill : _AttackSkills)
+	for (int i = 0; i < (int)en_SkillCharacteristicCount::SKILL_CHARACTERISTIC_MAX_COUNT; i++)
 	{
-		G_ObjectManager->SkillInfoReturn(AttackSkill->GetSkillInfo()->SkillMediumCategory, AttackSkill->GetSkillInfo());
-		G_ObjectManager->SkillReturn(AttackSkill);
+		_SkillCharacteristics[i].CharacteristicEmpty();
 	}
-
-	for (CSkill* TacTicSkill : _TacTicSkills)
-	{
-		G_ObjectManager->SkillInfoReturn(TacTicSkill->GetSkillInfo()->SkillMediumCategory, TacTicSkill->GetSkillInfo());
-		G_ObjectManager->SkillReturn(TacTicSkill);
-	}
-
-	for (CSkill* BufSkill : _BufSkills)
-	{
-		G_ObjectManager->SkillInfoReturn(BufSkill->GetSkillInfo()->SkillMediumCategory, BufSkill->GetSkillInfo());
-		G_ObjectManager->SkillReturn(BufSkill);
-	}
-
-	_AttackSkills.clear();
-	_TacTicSkills.clear();
-	_BufSkills.clear();
-}
-
-vector<CSkill*> CSkillBox::GetAttackSkill()
-{
-	return _AttackSkills;
-}
-
-vector<CSkill*> CSkillBox::GetTacTicSkill()
-{
-	return _TacTicSkills;
-}
-
-vector<CSkill*> CSkillBox::GetBufSkill()
-{
-	return _BufSkills;
 }
 
 vector<CSkill*> CSkillBox::GetGlobalSkills(en_SkillType ExceptSkillType, en_SkillKinds SkillKind)
@@ -154,76 +97,20 @@ vector<CSkill*> CSkillBox::GetGlobalSkills(en_SkillType ExceptSkillType, en_Skil
 
 	// 기본 공격 스킬은 제외함
 
-	// 요청한 스킬과 같은 종류의 스킬을 가져옴
-	for (CSkill* Skill : _AttackSkills)
+	// 요청한 스킬과 같은 종류의 스킬을 스킬특성 창에서 찾음
+	for (int i = 0; i < (int)en_SkillCharacteristicCount::SKILL_CHARACTERISTIC_MAX_COUNT; i++)
 	{
-		if (Skill->GetSkillInfo()->SkillType != en_SkillType::SKILL_DEFAULT_ATTACK 
-			&& Skill->GetSkillInfo()->SkillType != ExceptSkillType
-			&& Skill->GetSkillKind() == SkillKind
-			&& Skill->GetSkillInfo()->CanSkillUse == true)
+		vector<CSkill*> SkillCharacteristicActiveSkill = _SkillCharacteristics[i].GetActiveSkill();
+		for (CSkill* ActiveSkill : SkillCharacteristicActiveSkill)
 		{
-			GlobalSkills.push_back(Skill);
+			if (ActiveSkill->GetSkillInfo()->SkillType != ExceptSkillType
+				&& ActiveSkill->GetSkillKind() == SkillKind
+				&& ActiveSkill->GetSkillInfo()->CanSkillUse == true)
+			{
+				GlobalSkills.push_back(ActiveSkill);
+			}
 		}
-	}
-
-	for (CSkill* Skill : _TacTicSkills)
-	{
-		if (Skill->GetSkillInfo()->SkillType != ExceptSkillType
-			&& Skill->GetSkillKind() == SkillKind
-			&& Skill->GetSkillInfo()->CanSkillUse == true)
-		{
-			GlobalSkills.push_back(Skill);
-		}
-	}
-
-	for (CSkill* Skill : _BufSkills)
-	{
-		if (Skill->GetSkillInfo()->SkillType != ExceptSkillType
-			&& Skill->GetSkillKind() == SkillKind
-			&& Skill->GetSkillInfo()->CanSkillUse == true)
-		{
-			GlobalSkills.push_back(Skill);
-		}
-	}
+	}	
 
 	return GlobalSkills;
-}
-
-CSkill* CSkillBox::FindAttackSkill(en_SkillType FindAttackSkillType)
-{
-	for (int SlotIndex = 0; SlotIndex < _AttackSkills.size(); SlotIndex++)
-	{
-		if (_AttackSkills[SlotIndex]->GetSkillInfo()->SkillType == FindAttackSkillType)
-		{
-			return _AttackSkills[SlotIndex];
-		}
-	}
-
-	return nullptr;
-}
-
-CSkill* CSkillBox::FindTacTicSkill(en_SkillType FindTacTicSkillType)
-{
-	for (int SlotIndex = 0; SlotIndex < _TacTicSkills.size(); SlotIndex++)
-	{
-		if (_TacTicSkills[SlotIndex]->GetSkillInfo()->SkillType == FindTacTicSkillType)
-		{
-			return _TacTicSkills[SlotIndex];
-		}
-	}
-
-	return nullptr;
-}
-
-CSkill* CSkillBox::FindBufSkill(en_SkillType FindBufSkillType)
-{
-	for (int SlotIndex = 0; SlotIndex < _BufSkills.size(); SlotIndex++)
-	{
-		if (_BufSkills[SlotIndex]->GetSkillInfo()->SkillType == FindBufSkillType)
-		{
-			return _BufSkills[SlotIndex];
-		}
-	}
-
-	return nullptr;
 }
--- GameServer/SkillBox.h
+++ GameServer/SkillBox.h
@@ -1,46 +1,30 @@
 #pragma once
-#include"GameObjectInfo.h"
-
-class CSkill;
-
+#include "GameObjectInfo.h"
+#include "SkillCharacteristic.h"
 class CSkillBox
 {
 public:
 	CSkillBox();
 	~CSkillBox();
 
-	void Init();
+	void Init();	
 
-	// 공격 스킬 추가
-	void AddAttackSkill(CSkill* AttackSkillInfo);
-	// 전술 스킬 추가
-	void AddTacTicSkill(CSkill* TacTicSkillInfo);
-	// 강화 효과 스킬 추가
-	void AddBufSkill(CSkill* BufSkillInfo);
+	CSkillCharacteristic* FindCharacteristic(int8 FindCharacteristicIndex, int8 FindCharacteristicType);
+	void CreateChracteristic(int8 ChracteristicIndex, int8 CharacteristicType);
 
-	CSkill* FindSkill(en_SkillType SkillType);
+	CSkill* FindSkill(en_SkillCharacteristic CharacteristicType, en_SkillType SkillType);
 
 	void Update();
 
-	void Empty();
-
-	vector<CSkill*> GetAttackSkill();
-	vector<CSkill*> GetTacTicSkill();
-	vector<CSkill*> GetBufSkill();
+	void Empty();	
 
 	vector<CSkill*> GetGlobalSkills(en_SkillType ExceptSkillType, en_SkillKinds SkillKind);
 private:
-	//------------------------------
-	// 스킬박스가 소유중인 스킬 목록
-	//------------------------------
-	vector<CSkill*> _AttackSkills;
-	vector<CSkill*> _TacTicSkills;
-	vector<CSkill*> _BufSkills;
+	enum en_SkillCharacteristicCount
+	{
+		SKILL_CHARACTERISTIC_MAX_COUNT = 3
+	};
 
-	// 공격 스킬 찾기
-	CSkill* FindAttackSkill(en_SkillType FindAttackSkillType);
-	// 전술 스킬 찾기
-	CSkill* FindTacTicSkill(en_SkillType FindTacTicSkillType);
-	// 강화 효과 스킬 찾기
-	CSkill* FindBufSkill(en_SkillType FindBufSkillType);
+	CSkillCharacteristic _SkillCharacteristicPublic;
+	CSkillCharacteristic _SkillCharacteristics[SKILL_CHARACTERISTIC_MAX_COUNT];	
 };
