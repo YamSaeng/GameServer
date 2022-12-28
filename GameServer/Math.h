d58d26c246c1f3513456c5cc5fe929d29fdc34b6
정성원 <tyhg56@naver.com>
2022-12-28 18:13:26 +0900

    Fix : " Math 수정 "
    GameServer
     - Math 수정
      - CalculateMeleeDamage 수정
       - InOutCritical 변수로 true일때 크리티컬 검사, false일때 크리티컬 검사를 하지 않음

--- GameServer/Math.h
+++ GameServer/Math.h
@@ -1,33 +1,43 @@
 #pragma once
 #include <atlbase.h>
+#include "GameObjectInfo.h"
 
 class CMath
 {
 public:
-	static int32 CalculateMeleeDamage(int32 TargetDefence, int32 MinDamage, int32 MaxDamage, int16 CriticalPoint, bool *OutCritical)
+	static int32 CalculateMeleeDamage(bool* InOutCritical, int32 TargetDefence, int32 MinDamage, int32 MaxDamage, int16 CriticalPoint)
 	{
 		random_device Seed;
 		default_random_engine Eng(Seed());		
 
-		// 크리티컬 판단
-		float CriticalPointCheck = CriticalPoint / 1000.0f;
-		bernoulli_distribution CriticalCheck(CriticalPointCheck);
-		bool IsCritical = CriticalCheck(Eng);
-
-		*OutCritical = IsCritical;
-
 		mt19937 Gen(Seed());
-		uniform_int_distribution<int> DamageChoiceRandom( MinDamage, MaxDamage );
+		uniform_int_distribution<int> DamageChoiceRandom(MinDamage, MaxDamage);
 
 		int32 ChoiceRandomDamage = DamageChoiceRandom(Gen);
 
-		int32 CriticalDamage = IsCritical ? ChoiceRandomDamage * 2 : ChoiceRandomDamage;
+		int32 CriticalDamage = 0;
+
+		if (*InOutCritical == true)
+		{
+			// 크리티컬 판단
+			float CriticalPointCheck = CriticalPoint / 1000.0f;
+			bernoulli_distribution CriticalCheck(CriticalPointCheck);
+			bool IsCritical = CriticalCheck(Eng);
+
+			*InOutCritical = IsCritical;
+
+			CriticalDamage = IsCritical ? ChoiceRandomDamage * 2 : ChoiceRandomDamage;
+		}				
+		else
+		{
+			CriticalDamage = ChoiceRandomDamage;
+		}		
 
 		float DefenceRate = (float)pow(((float)(200 - TargetDefence)) / 20, 2) * 0.01f;
 
 		int32 FinalDamage = (int32)(CriticalDamage * DefenceRate);
 
 		return FinalDamage;
-	}
+	}	
 };
 
