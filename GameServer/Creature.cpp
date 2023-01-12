b15bd45558674a0ee253458fbacdb2a0afe31019
정성원 <tyhg56@naver.com>
2023-01-12 17:11:24 +0900

    Feat : " Creature  추가 "
    GameServer
     - Creature 추가
      - GameObject를 생물과 비생물로 구분

---
+++ GameServer/Creature.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "Creature.h"
+#include "DataManager.h"
+#include "ObjectManager.h"
+
+CInventoryManager CCreature::GetInventoryManager()
+{
+    return _Inventory;
+}
+
+void CCreature::NPCInit(en_NonPlayerType NonPlayerType)
+{
+	switch (NonPlayerType)
+	{
+	case en_NonPlayerType::NPC_일반_상인:
+		{
+			for (auto MerchantItemIter : G_Datamanager->_GeneralMerchantItems)
+			{
+				CItem* MerchantItem = G_ObjectManager->ItemCreate((en_SmallItemCategory)MerchantItemIter.first);
+				_Inventory.DBItemInsertItem(0, MerchantItem);
+			}
+		}		
+		break;
+	default:
+		break;
+	}
+}
---
+++ GameServer/Creature.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GameObject.h"
+#include "InventoryManager.h"
+
+class CCreature : public CGameObject
+{
+public:
+	//--------------------------------------------
+	// 가방 반환
+	//--------------------------------------------
+	CInventoryManager GetInventoryManager();
+
+	//--------------------------------------------
+	// NPC 초기화
+	//--------------------------------------------
+	void NPCInit(en_NonPlayerType NonPlayerType);
+protected:
+	CInventoryManager _Inventory;
+};
+
