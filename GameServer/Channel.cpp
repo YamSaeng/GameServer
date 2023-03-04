59586e9e300446da14b2279d087581b13aa57d97
정성원 <tyhg56@naver.com>
2023-03-04 17:48:38 +0900

    Style : " Channel 수정 "
    GameServer
     - Channel
      - CHANNEL_ENVIRONMENT_MAX 100 -> 300 수정

--- GameServer/Channel.cpp
+++ GameServer/Channel.cpp
@@ -453,6 +453,7 @@ void CChannel::Update()
 							}
 						}
 						break;	
+					case en_GameObjectType::OBJECT_WALL:
 					case en_GameObjectType::OBJECT_STONE:
 					case en_GameObjectType::OBJECT_TREE:
 						{
@@ -932,12 +933,7 @@ CGameObject* CChannel::FindChannelObject(int64 ObjectID, en_GameObjectType GameO
 			}
 		}
 		break;	
-	case en_GameObjectType::OBJECT_LEFT_RIGHT_WALL:
-	case en_GameObjectType::OBJECT_UP_DOWN_WALL:
-	case en_GameObjectType::OBJECT_UP_TO_LEFT_WALL:
-	case en_GameObjectType::OBJECT_UP_TO_RIGHT_WALL:
-	case en_GameObjectType::OBJECT_DOWN_TO_LEFT_WALL:
-	case en_GameObjectType::OBJECT_DOWN_TO_RIGHT_WALL:
+	case en_GameObjectType::OBJECT_WALL:
 	case en_GameObjectType::OBJECT_STONE:
 	case en_GameObjectType::OBJECT_TREE:
 		{
@@ -1046,12 +1042,7 @@ vector<CGameObject*> CChannel::FindChannelObjects(en_GameObjectType GameObjectTy
 			}
 		}
 		break;	
-	case en_GameObjectType::OBJECT_LEFT_RIGHT_WALL:
-	case en_GameObjectType::OBJECT_UP_DOWN_WALL:
-	case en_GameObjectType::OBJECT_UP_TO_LEFT_WALL:
-	case en_GameObjectType::OBJECT_UP_TO_RIGHT_WALL:
-	case en_GameObjectType::OBJECT_DOWN_TO_LEFT_WALL:
-	case en_GameObjectType::OBJECT_DOWN_TO_RIGHT_WALL:
+	case en_GameObjectType::OBJECT_WALL:
 	case en_GameObjectType::OBJECT_STONE:
 	case en_GameObjectType::OBJECT_TREE:
 		for (int32 i = 0; i < en_Channel::CHANNEL_ENVIRONMENT_MAX; i++)
@@ -1179,12 +1170,7 @@ vector<CGameObject*> CChannel::FindChannelObjects(vector<st_FieldOfViewInfo>& Fi
 				}
 			}
 			break;		
-		case en_GameObjectType::OBJECT_LEFT_RIGHT_WALL:
-		case en_GameObjectType::OBJECT_UP_DOWN_WALL:
-		case en_GameObjectType::OBJECT_UP_TO_LEFT_WALL:
-		case en_GameObjectType::OBJECT_UP_TO_RIGHT_WALL:
-		case en_GameObjectType::OBJECT_DOWN_TO_LEFT_WALL:
-		case en_GameObjectType::OBJECT_DOWN_TO_RIGHT_WALL:
+		case en_GameObjectType::OBJECT_WALL:
 		case en_GameObjectType::OBJECT_STONE:
 		case en_GameObjectType::OBJECT_TREE:
 			{
@@ -1789,12 +1775,7 @@ bool CChannel::EnterChannel(CGameObject* EnterChannelGameObject, st_Vector2Int* 
 			}
 		}
 		break;
-	case en_GameObjectType::OBJECT_LEFT_RIGHT_WALL:
-	case en_GameObjectType::OBJECT_UP_DOWN_WALL:
-	case en_GameObjectType::OBJECT_UP_TO_LEFT_WALL:
-	case en_GameObjectType::OBJECT_UP_TO_RIGHT_WALL:
-	case en_GameObjectType::OBJECT_DOWN_TO_LEFT_WALL:
-	case en_GameObjectType::OBJECT_DOWN_TO_RIGHT_WALL:
+	case en_GameObjectType::OBJECT_WALL:	
 	case en_GameObjectType::OBJECT_STONE:
 	case en_GameObjectType::OBJECT_TREE:
 		{
@@ -1802,9 +1783,11 @@ bool CChannel::EnterChannel(CGameObject* EnterChannelGameObject, st_Vector2Int* 
 			if (EnterChannelEnvironment != nullptr)
 			{
 				EnterChannelEnvironment->_GameObjectInfo.ObjectPositionInfo.CollisionPosition = SpawnPosition;
-				EnterChannelEnvironment->_GameObjectInfo.ObjectPositionInfo.Position._X = EnterChannelEnvironment->_GameObjectInfo.ObjectPositionInfo.CollisionPosition._X;
+				EnterChannelEnvironment->_GameObjectInfo.ObjectPositionInfo.Position._X = EnterChannelEnvironment->_GameObjectInfo.ObjectPositionInfo.CollisionPosition._X + 0.5f;
 				EnterChannelEnvironment->_GameObjectInfo.ObjectPositionInfo.Position._Y = EnterChannelEnvironment->_GameObjectInfo.ObjectPositionInfo.CollisionPosition._Y + 0.5f;				
 
+				EnterChannelEnvironment->GetRectCollision()->SetActive(true);
+				EnterChannelEnvironment->GetRectCollision()->Update();
 				EnterChannelEnvironment->Start();
 
 				// 환경 오브젝트 저장
@@ -1919,12 +1902,7 @@ void CChannel::LeaveChannel(CGameObject* LeaveChannelGameObject)
 
 		_ChannelItemArrayIndexs.Push(LeaveChannelGameObject->_ChannelArrayIndex);
 		break;
-	case en_GameObjectType::OBJECT_LEFT_RIGHT_WALL:
-	case en_GameObjectType::OBJECT_UP_DOWN_WALL:
-	case en_GameObjectType::OBJECT_UP_TO_LEFT_WALL:
-	case en_GameObjectType::OBJECT_UP_TO_RIGHT_WALL:
-	case en_GameObjectType::OBJECT_DOWN_TO_LEFT_WALL:
-	case en_GameObjectType::OBJECT_DOWN_TO_RIGHT_WALL:
+	case en_GameObjectType::OBJECT_WALL:
 	case en_GameObjectType::OBJECT_STONE:
 	case en_GameObjectType::OBJECT_TREE:
 		G_ObjectManager->ObjectReturn(LeaveChannelGameObject);
