7b5c28fc093002251226d40d386edcca020d7b7e
정성원 <tyhg56@naver.com>
2023-03-12 13:42:35 +0900

    Fix : " Math 수정 "
    GameServer
     - Math
      - CalculateDamage() 수정
       - 데미지 공식 변경
       - Str, Dex, Int, Luck 계산 추가
       - SkillType에 따라 데미지 다르게 계산

--- GameServer/Math.h
+++ GameServer/Math.h
@@ -5,7 +5,12 @@
 class CMath
 {
 public:
-	static int32 CalculateMeleeDamage(bool* InOutCritical, int32 TargetDefence, int32 MinDamage, int32 MaxDamage, int16 CriticalPoint)
+	static int32 CalculateDamage(en_SkillType SkillType,
+		int32& Str, int32& Dex, int32& Int, int32& Luck, 
+		bool* InOutCritical,
+		int32 TargetDefence, 
+		int32 MinDamage, int32 MaxDamage, 
+		int16 CriticalPoint)
 	{
 		random_device Seed;
 		default_random_engine Eng(Seed());		
@@ -33,9 +38,43 @@ public:
 			CriticalDamage = ChoiceRandomDamage;
 		}		
 
-		float DefenceRate = (float)pow(((float)(200 - TargetDefence)) / 20, 2) * 0.01f;
+		int32 FinalDamage = 0;
 
-		int32 FinalDamage = (int32)(CriticalDamage * DefenceRate);
+		switch (SkillType)
+		{				
+		case en_SkillType::SKILL_DEFAULT_ATTACK:
+		case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_FIERCE_ATTACK:
+		case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_CONVERSION_ATTACK:
+		case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_SHAHONE:
+		case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_CHOHONE:
+		case en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SHIELD_SMASH:
+		case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_QUICK_CUT:
+		case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_FAST_CUT:
+		case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_ATTACK:
+		case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_STEP:
+		case en_SkillType::SKILL_ASSASSINATION_ACTIVE_BUF_WEAPON_POISON:
+		case en_SkillType::SKILL_GOBLIN_ACTIVE_MELEE_DEFAULT_ATTACK:
+
+			FinalDamage = (CriticalDamage + Str / 2) * (1 - ((float)TargetDefence / (100.0f + (float)TargetDefence)));
+
+			break;
+		case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_FLAME_HARPOON:
+		case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ICE_CHAIN:
+		case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ICE_WAVE:
+		case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_LIGHTNING_STRIKE:
+		case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_HEL_FIRE:
+		case en_SkillType::SKILL_SPELL_ACTIVE_BUF_TELEPORT:
+		case en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_DIVINE_STRIKE:
+
+			FinalDamage = (CriticalDamage + Int / 2) * (1 - ((float)TargetDefence / (100.0f + (float)TargetDefence)));
+
+			break;		
+		case en_SkillType::SKILL_SHOOTING_ACTIVE_ATTACK_SNIFING:
+			break;				
+		}		
+
+		/*float DefenceRate = (float)pow(((float)(200 - 1)) / 20, 2) * 0.01f;
+		int32 FinalDamage = (int32)(CriticalDamage * DefenceRate);*/
 
 		return FinalDamage;
 	}	
