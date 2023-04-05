2fc39a3b192f51cf62331c937ba5b43f89fc0d9c
정성원 <tyhg56@naver.com>
2023-04-05 23:41:23 +0900

    Feat : " SwordBlade 추가 "
    GameServer
     - SwordBlade 추가
      - 칼날 날리기 ( 칼날 객체 )

---
+++ GameServer/SwordBlade.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "SwordBlade.h"
+
+CSwordBlade::CSwordBlade()
+{
+}
+
+CSwordBlade::~CSwordBlade()
+{
+}
---
+++ GameServer/SwordBlade.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "GameObject.h"
+
+class CSwordBlade : public CGameObject
+{
+public:
+	CSwordBlade();
+	~CSwordBlade();
+
+private:
+};
+
