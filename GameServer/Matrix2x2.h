d7e70e88f6bedd6190eb9e03b904f83df49f31f8
정성원 <tyhg56@naver.com>
2023-04-10 22:53:10 +0900

    Fix : " Matrix2x2 수정 "
    GameServer
     - Matrix2x2
      - operator[] 추가
      - operator*(Vector2& Vector) 추가
      - Transpose() 추가 ( 행렬 전치 연산 )

--- GameServer/Matrix2x2.h
+++ GameServer/Matrix2x2.h
@@ -18,5 +18,22 @@ public:
 	}
 
 	array<Vector2, 2> Cols;
+
+	FORCEINLINE Vector2& operator[](BYTE Index)
+	{
+		return Cols[Index];
+	}
+
+	FORCEINLINE Vector2 operator*(Vector2& Vector) const
+	{
+		Matrix2x2 transposedMatrix = Transpose();
+		return Vector2(transposedMatrix[0].Dot(Vector),transposedMatrix[1].Dot(Vector));
+	}
+
+	// 전치 연산
+	FORCEINLINE Matrix2x2 Transpose() const
+	{
+		return Matrix2x2(Vector2(Cols[0].X, Cols[1].X),Vector2(Cols[0].Y, Cols[1].Y));
+	}	
 };
 
