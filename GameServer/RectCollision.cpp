2dd08de6556059be59d0578a92089580f111e3d7
정성원 <tyhg56@naver.com>
2023-04-13 16:14:20 +0900

    Fix : " RectCollision 수정 "
    GameServer
     - RectCollision
      - _Direction 추가 ( RectCollision 회전 방향 )
      - _MiddlePosition 추가 ( RectCollision 중심 좌표 )
      - _LeftTop, _RightTop, _Leftdown, _RightDown 추가 ( 왼쪽위, 오른쪽위, 왼쪽아래, 오른쪽 아래 꼭짓점 좌표 )
      - Init() 수정
       - RectCollision을 생성할때 회전 값을 Vector값으로 받음
      - PositionUpdate() 추가
       - _MiddlePosition 계산
       - _LeftTop, _LeftDown, _RightTop, _RightDown 계산
      - RotationUpdate()
       - _LeftTop, _RightTop, _LeftDown, _RightDown을 중심좌표 기준으로 회전 방향 만큼 회전시켜줌

--- GameServer/RectCollision.cpp
+++ GameServer/RectCollision.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "RectCollision.h"
 #include "GameObject.h"
+#include "Matrix2x2.h"
 
 CRectCollision::CRectCollision()
 {
@@ -12,7 +13,7 @@ CRectCollision::~CRectCollision()
 
 }
 
-void CRectCollision::Init(CGameObject* OwnerObject)
+void CRectCollision::Init(CGameObject* OwnerObject, Vector2 Direction)
 {
 	_RectCollisionActive = true;
 
@@ -20,6 +21,8 @@ void CRectCollision::Init(CGameObject* OwnerObject)
 
 	if (_OwnerObject != nullptr)
 	{
+		_Direction = Direction;
+
 		switch (_OwnerObject->_GameObjectInfo.ObjectType)
 		{
 		case en_GameObjectType::OBJECT_PLAYER:
@@ -45,6 +48,9 @@ void CRectCollision::Init(CGameObject* OwnerObject)
 			_Size.Y = 0.5f;
 			break;
 		}
+
+		PositionUpdate();
+		RotateUpdate();		
 	}
 	else
 	{
@@ -63,20 +69,62 @@ bool CRectCollision::IsCollision(CRectCollision* ARectCollision, CRectCollision*
 void CRectCollision::SetActive(bool _Active)
 {
 	_RectCollisionActive = _Active;
+}
 
-	Update();
+void CRectCollision::PositionUpdate()
+{
+	_Position = _OwnerObject->_GameObjectInfo.ObjectPositionInfo.Position;
+
+	_MiddlePosition.X = _Position.X + _Size.X / 2.0f;
+	_MiddlePosition.Y = _Position.Y - _Size.Y / 2.0f;
+
+	_LeftTop = _Position;
+
+	_LeftDown.X = _Position.X;
+	_LeftDown.Y = _Position.Y - _Size.Y;
+
+	_RightTop.X = _Position.X + _Size.X;
+	_RightTop.Y = _Position.Y;
+
+	_RightDown.X = _Position.X + _Size.X;
+	_RightDown.Y = _Position.Y - _Size.Y;
+}
+
+void CRectCollision::RotateUpdate()
+{
+	float Degree = _Direction.AngleToDegree();
+	float Sin = 0.0f; float Cos = 0.0f;
+	Math::GetSinCos(Sin, Cos, Degree);
+
+	// 회전 행렬 생성
+	Vector2 Basis1(Cos, Sin);
+	Vector2 Basis2(-Sin, Cos);
+	Matrix2x2 RotationMatrix(Basis1, Basis2);
+
+	// 중심 좌표 기준으로 회전
+	Vector2 LeftTopRot = RotationMatrix * (_LeftTop - _MiddlePosition);
+	Vector2 LeftDown = RotationMatrix * (_LeftDown - _MiddlePosition);
+	Vector2 RightTop = RotationMatrix * (_RightTop - _MiddlePosition);
+	Vector2 RightDown = RotationMatrix * (_RightDown - _MiddlePosition);
+
+	// 중심 좌표로 이동
+	_LeftTop = LeftTopRot + _MiddlePosition;
+	_LeftDown = LeftDown + _MiddlePosition;
+	_RightTop = RightTop + _MiddlePosition;
+	_RightDown = RightDown + _MiddlePosition;
 }
 
 void CRectCollision::Update()
 {
 	if (_OwnerObject != nullptr)
 	{
-		_Position = _OwnerObject->_GameObjectInfo.ObjectPositionInfo.Position;
+		PositionUpdate();
+		RotateUpdate();		
 	}
 	else
 	{
 		CRASH("OwnerObject null");
-	}	
+	}
 }
 
 bool CRectCollision::GetActive()
--- GameServer/RectCollision.h
+++ GameServer/RectCollision.h
@@ -5,20 +5,31 @@ class CGameObject;
 
 class CRectCollision
 {
-public:	
+public:
 	Vector2 _Position;
 	Vector2 _Size;
 
-	CRectCollision();	
+	Vector2 _Direction;
+
+	Vector2 _MiddlePosition;
+
+	Vector2 _LeftTop;
+	Vector2 _RightTop;
+	Vector2 _LeftDown;
+	Vector2 _RightDown;
+
+	CRectCollision();
 	~CRectCollision();
 
-	void Init(CGameObject* OwnerObject);
+	void Init(CGameObject* OwnerObject, Vector2 Direction);
 
 	static bool IsCollision(CRectCollision* ARectCollision, CRectCollision* BRectCollision);
 
 	bool GetActive();
-	void SetActive(bool _Active);	
+	void SetActive(bool _Active);
 
+	void PositionUpdate();
+	void RotateUpdate();
 	void Update();
 private:
 	CGameObject* _OwnerObject;
