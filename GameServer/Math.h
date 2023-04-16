ec872620377f86cbad85a39556448fa55982c59b
정성원 <tyhg56@naver.com>
2023-04-16 16:55:24 +0900

    Fix : " Math 수정 "
    GameServer
     - Math
      - DegreeToRadian() 추가 ( 각도 -> 라디안 변환 )
      - RadianToDegree() 추가 ( 라디안 -> 각도 변환 )
      - GetSinCosRadian() 추가 ( 라디안 값으로 Sin, Cos 얻기 )
      - GetSinCosDegree() 추가 ( 각도 값으로 Sin, Cos 얻기 )

--- GameServer/Math.h
+++ GameServer/Math.h
@@ -1,82 +1,96 @@
 #pragma once
 #include <atlbase.h>
-#include "GameObjectInfo.h"
 
-class CMath
+struct Math
 {
 public:
-	static int32 CalculateDamage(en_SkillType SkillType,
-		int32& Str, int32& Dex, int32& Int, int32& Luck, 
-		bool* InOutCritical,
-		int32 TargetDefence, 
-		int32 MinDamage, int32 MaxDamage, 
-		int16 CriticalPoint)
+
+	static constexpr float PI = { 3.14159265358979323846f };
+	static constexpr float InvPI = { 0.31830988618f };
+	static constexpr float HalfPI = { 1.57079632679f };		
+
+	// 각도 -> 라디안
+	FORCEINLINE static float DegreeToRadian(float Degree)
 	{
-		random_device Seed;
-		default_random_engine Eng(Seed());		
+		return Degree * PI / 180.0f;
+	}
 
-		mt19937 Gen(Seed());
-		uniform_int_distribution<int> DamageChoiceRandom(MinDamage, MaxDamage);
-
-		int32 ChoiceRandomDamage = DamageChoiceRandom(Gen);
-
-		int32 CriticalDamage = 0;
-
-		if (*InOutCritical == true)
+	// 라디안 -> 각도
+	FORCEINLINE static float RadianToDegree(float Radian)
+	{
+		return Radian * 180.0f * InvPI;
+	}
+	
+	// Sin Cos 얻기 ( 라디안 값 )
+	FORCEINLINE static void GetSinCosRadian(float& OutSin, float& OutCos, float InRadian)
+	{
+		// Copied from UE4 Source Code
+		// Map Value to y in [-pi,pi], x = 2*pi*quotient + remainder.
+		float quotient = (InvPI * 0.5f) * InRadian;
+		if (InRadian >= 0.0f)
 		{
-			// 크리티컬 판단
-			float CriticalPointCheck = CriticalPoint / 1000.0f;
-			bernoulli_distribution CriticalCheck(CriticalPointCheck);
-			bool IsCritical = CriticalCheck(Eng);
-
-			*InOutCritical = IsCritical;
-
-			CriticalDamage = IsCritical ? ChoiceRandomDamage * 2 : ChoiceRandomDamage;
-		}				
+			quotient = (float)((int)(quotient + 0.5f));
+		}
 		else
 		{
-			CriticalDamage = ChoiceRandomDamage;
-		}		
+			quotient = (float)((int)(quotient - 0.5f));
+		}
+		float y = InRadian - (2.0f * PI) * quotient;
 
-		int32 FinalDamage = 0;
+		// Map y to [-pi/2,pi/2] with sin(y) = sin(Value).
+		float sign = 0.f;
+		if (y > HalfPI)
+		{
+			y = PI - y;
+			sign = -1.0f;
+		}
+		else if (y < -HalfPI)
+		{
+			y = -PI - y;
+			sign = -1.0f;
+		}
+		else
+		{
+			sign = +1.0f;
+		}
 
-		switch (SkillType)
-		{				
-		case en_SkillType::SKILL_DEFAULT_ATTACK:
-		case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_FIERCE_ATTACK:
-		case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_CONVERSION_ATTACK:
-		case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_SHAHONE:
-		case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_CHOHONE:
-		case en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SHIELD_SMASH:
-		case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_QUICK_CUT:
-		case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_FAST_CUT:
-		case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_ATTACK:
-		case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_STEP:
-		case en_SkillType::SKILL_ASSASSINATION_ACTIVE_BUF_WEAPON_POISON:
-		case en_SkillType::SKILL_GOBLIN_ACTIVE_MELEE_DEFAULT_ATTACK:
+		float y2 = y * y;
 
-			FinalDamage = (CriticalDamage + Str / 2) * (1 - ((float)TargetDefence / (100.0f + (float)TargetDefence)));
+		// 11-degree minimax approximation
+		OutSin = (((((-2.3889859e-08f * y2 + 2.7525562e-06f) * y2 - 0.00019840874f) * y2 + 0.0083333310f) * y2 - 0.16666667f) * y2 + 1.0f) * y;
 
-			break;
-		case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_FLAME_HARPOON:
-		case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ICE_CHAIN:
-		case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ICE_WAVE:
-		case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_LIGHTNING_STRIKE:
-		case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_HEL_FIRE:
-		case en_SkillType::SKILL_SPELL_ACTIVE_BUF_TELEPORT:
-		case en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_DIVINE_STRIKE:
+		// 10-degree minimax approximation
+		float p = ((((-2.6051615e-07f * y2 + 2.4760495e-05f) * y2 - 0.0013888378f) * y2 + 0.041666638f) * y2 - 0.5f) * y2 + 1.0f;
+		OutCos = sign * p;
+	}
 
-			FinalDamage = (CriticalDamage + Int / 2) * (1 - ((float)TargetDefence / (100.0f + (float)TargetDefence)));
-
-			break;		
-		case en_SkillType::SKILL_SHOOTING_ACTIVE_ATTACK_SNIFING:
-			break;				
-		}		
-
-		/*float DefenceRate = (float)pow(((float)(200 - 1)) / 20, 2) * 0.01f;
-		int32 FinalDamage = (int32)(CriticalDamage * DefenceRate);*/
-
-		return FinalDamage;
-	}	
+	// SinCos 얻기 ( 각도 값 )
+	FORCEINLINE static void GetSinCosDegree(OUT float& Sin, OUT float& Cos, IN float Degree)
+	{
+		if (Degree == 0.f)
+		{
+			Sin = 0.f;
+			Cos = 1.f;
+		}
+		else if (Degree == 90.f)
+		{
+			Sin = 1.f;
+			Cos = 0.f;
+		}
+		else if (Degree == 180.f)
+		{
+			Sin = 0.f;
+			Cos = -1.f;
+		}
+		else if (Degree == 270.f)
+		{
+			Sin = -1.f;
+			Cos = 0.f;
+		}
+		else
+		{
+			GetSinCosRadian(Sin, Cos, Math::DegreeToRadian(Degree));
+		}
+	}
 };
 
