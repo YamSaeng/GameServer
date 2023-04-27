8237d461fca56f3c2b41f76a80154eaf5abb8ff5
정성원 <tyhg56@naver.com>
2023-04-27 23:38:58 +0900

    Fix : " SkillCharacteristic 수정 "
    GameServer
     - SkillCharacteristic
      - SkillCharacteristicActive() 수정
       - 배우고자 하는 SkillType에 따라 배움 ( 연속기 스킬 배울 경우 모든 연속기 스킬 배움 )

--- GameServer/SkillCharacteristic.cpp
+++ GameServer/SkillCharacteristic.cpp
@@ -44,7 +44,7 @@ void CSkillCharacteristic::SkillCharacteristicInit(en_SkillCharacteristic SkillC
 	case en_SkillCharacteristic::SKILL_CATEGORY_FIGHT:
 		{	
 			int16 StartFightActiveSkillType = (int16)en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_FIERCE_ATTACK;
-			int16 EndFightActiveSkilltype = (int16)en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SHIELD_SMASH;
+			int16 EndFightActiveSkilltype = (int16)en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_POWERFUL_ATTACK;
 	
 			// 격투 액티브 스킬 생성
 			for (int16 i = StartFightActiveSkillType; i < EndFightActiveSkilltype; i++)
@@ -60,8 +60,8 @@ void CSkillCharacteristic::SkillCharacteristicInit(en_SkillCharacteristic SkillC
 		break;
 	case en_SkillCharacteristic::SKILL_CATEGORY_PROTECTION:
 		{
-			int16 StartFightPassiveSkillType = (int16)en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SHIELD_SMASH;
-			int16 EndFightPassiveSkillType = (int16)en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_FLAME_HARPOON;
+			int16 StartFightPassiveSkillType = (int16)en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_POWERFUL_ATTACK;
+			int16 EndFightPassiveSkillType = (int16)en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_FLAME_BOLT;
 
 			for (int16 i = StartFightPassiveSkillType; i < EndFightPassiveSkillType; i++)
 			{
@@ -76,7 +76,7 @@ void CSkillCharacteristic::SkillCharacteristicInit(en_SkillCharacteristic SkillC
 		break;
 	case en_SkillCharacteristic::SKILL_CATEGORY_SPELL:
 		{
-			int16 StartFightPassiveSkillType = (int16)en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_FLAME_HARPOON;
+			int16 StartFightPassiveSkillType = (int16)en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_FLAME_BOLT;
 			int16 EndFightPassiveSkillType = (int16)en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_DIVINE_STRIKE;
 
 			for (int16 i = StartFightPassiveSkillType; i < EndFightPassiveSkillType; i++)
@@ -89,23 +89,7 @@ void CSkillCharacteristic::SkillCharacteristicInit(en_SkillCharacteristic SkillC
 				_ActiveSkills.push_back(ActiveSkill);
 			}
 		}
-		break;
-	case en_SkillCharacteristic::SKILL_CATEGORY_SHOOTING:
-		{
-			int16 StartFightPassiveSkillType = (int16)en_SkillType::SKILL_SHOOTING_ACTIVE_ATTACK_SNIFING;
-			int16 EndFightPassiveSkillType = (int16)en_SkillType::SKILL_GOBLIN_ACTIVE_MELEE_DEFAULT_ATTACK;
-
-			for (int16 i = StartFightPassiveSkillType; i < EndFightPassiveSkillType; i++)
-			{
-				CSkill* ActiveSkill = G_ObjectManager->SkillCreate();
-
-				st_SkillInfo* ActiveSkillInfo = G_Datamanager->FindSkillData((en_SkillType)i);
-				ActiveSkill->SetSkillInfo(en_SkillCategory::SKILL_CATEGORY_ACTIVE_SKILL, ActiveSkillInfo);
-
-				_ActiveSkills.push_back(ActiveSkill);
-			}
-		}
-		break;
+		break;	
 	case en_SkillCharacteristic::SKILL_CATEGORY_DISCIPLINE:
 		{
 			int16 StartFightPassiveSkillType = (int16)en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_DIVINE_STRIKE;
@@ -138,17 +122,178 @@ void CSkillCharacteristic::SkillCharacteristicInit(en_SkillCharacteristic SkillC
 			}
 		}
 		break;
+	case en_SkillCharacteristic::SKILL_CATEGORY_SHOOTING:
+		{
+			int16 StartFightPassiveSkillType = (int16)en_SkillType::SKILL_SHOOTING_ACTIVE_ATTACK_SNIFING;
+			int16 EndFightPassiveSkillType = (int16)en_SkillType::SKILL_GOBLIN_ACTIVE_MELEE_DEFAULT_ATTACK;
+
+			for (int16 i = StartFightPassiveSkillType; i < EndFightPassiveSkillType; i++)
+			{
+				CSkill* ActiveSkill = G_ObjectManager->SkillCreate();
+
+				st_SkillInfo* ActiveSkillInfo = G_Datamanager->FindSkillData((en_SkillType)i);
+				ActiveSkill->SetSkillInfo(en_SkillCategory::SKILL_CATEGORY_ACTIVE_SKILL, ActiveSkillInfo);
+
+				_ActiveSkills.push_back(ActiveSkill);
+			}
+		}
+		break;
 	}	
 }
 
 void CSkillCharacteristic::SkillCharacteristicActive(bool IsSkillLearn, en_SkillType SkillType, int8 SkillLevel)
 {
-	CSkill* ActiveSkill = FindSkill(SkillType);
-	if (ActiveSkill != nullptr)
-	{
-		ActiveSkill->GetSkillInfo()->IsSkillLearn = IsSkillLearn;
-		ActiveSkill->GetSkillInfo()->SkillLevel = SkillLevel;
+	vector<en_SkillType> LearnSkillTypes;
+
+	switch (SkillType)
+	{		
+	case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_FIERCE_ATTACK:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_FIERCE_ATTACK);
+		LearnSkillTypes.push_back(en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_CONVERSION_ATTACK);
+		LearnSkillTypes.push_back(en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_WRATH_ATTACK);
+		break;	
+	case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_SMASH_WAVE:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_SMASH_WAVE);
+		break;
+	case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_FLY_KNIFE:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_FLY_KNIFE);
+		LearnSkillTypes.push_back(en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_COMBO_FLY_KNIFE);
+		break;	
+	case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_JUMPING_ATTACK:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_JUMPING_ATTACK);
+		break;
+	case en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_PIERCING_WAVE:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_FIGHT_ACTIVE_ATTACK_PIERCING_WAVE);
+		break;
+	case en_SkillType::SKILL_FIGHT_ACTIVE_BUF_CHARGE_POSE:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_FIGHT_ACTIVE_BUF_CHARGE_POSE);
+		break;
+	case en_SkillType::SKILL_FIGHT_ACTIVE_BUF_COUNTER_ARMOR:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_FIGHT_ACTIVE_BUF_COUNTER_ARMOR);
+		break;
+	case en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_POWERFUL_ATTACK:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_POWERFUL_ATTACK);
+		LearnSkillTypes.push_back(en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SHARP_ATTACK);
+		LearnSkillTypes.push_back(en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_LAST_ATTACK);
+		break;	
+	case en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SHIELD_SMASH:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SHIELD_SMASH);
+		break;
+	case en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SHIELD_COUNTER:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SHIELD_COUNTER);
+		break;
+	case en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SWORD_STORM:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_SWORD_STORM);
+		break;
+	case en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_CAPTURE:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_PROTECTION_ACTIVE_ATTACK_CAPTURE);
+		break;
+	case en_SkillType::SKILL_PROTECTION_ACTIVE_BUF_FURY:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_PROTECTION_ACTIVE_BUF_FURY);
+		break;
+	case en_SkillType::SKILL_PROTECTION_ACTIVE_DOUBLE_ARMOR:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_PROTECTION_ACTIVE_DOUBLE_ARMOR);
+		break;
+	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_FLAME_BOLT:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_FLAME_BOLT);
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_FLAME_BLAZE);
+		break;	
+	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ICE_CHAIN:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ICE_CHAIN);
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ICE_WAVE);
+		break;	
+	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ROOT:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_ROOT);
+		break;
+	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_SLEEP:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_SLEEP);
+		break;
+	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_WINTER_BINDING:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_WINTER_BINDING);
+		break;
+	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_LIGHTNING_STRIKE:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_LIGHTNING_STRIKE);
+		break;
+	case en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_HEL_FIRE:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SPELL_ACTIVE_ATTACK_HEL_FIRE);
+		break;
+	case en_SkillType::SKILL_SPELL_ACTIVE_BUF_BACK_TELEPORT:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SPELL_ACTIVE_BUF_BACK_TELEPORT);
+		break;
+	case en_SkillType::SKILL_SPELL_ACTIVE_BUF_ILLUSION:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SPELL_ACTIVE_BUF_ILLUSION);
+		break;
+	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_DIVINE_STRIKE:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_DIVINE_STRIKE);
+		LearnSkillTypes.push_back(en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_THUNDER_BOLT);
+		break;	
+	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_ROOT:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_ROOT);
+		break;
+	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_JUDGMENT:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_DISCIPLINE_ACTIVE_ATTACK_JUDGMENT);
+		break;
+	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_HEALING_LIGHT:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_HEALING_LIGHT);
+		break;
+	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_RECOVERY_LIGHT:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_RECOVERY_LIGHT);
+		break;
+	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_VITALITY_LIGHT:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_VITALITY_LIGHT);
+		break;
+	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_HEALING_GRACE:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_HEALING_GRACE);
+		break;
+	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_HEALING_WIND:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_HEALING_WIND);
+		break;
+	case en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_RECOVERY_WIND:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_DISCIPLINE_ACTIVE_HEAL_RECOVERY_WIND);
+		break;
+	case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_QUICK_CUT:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_QUICK_CUT);
+		LearnSkillTypes.push_back(en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_FAST_CUT);
+		break;	
+	case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_ATTACK:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_ATTACK);
+		LearnSkillTypes.push_back(en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_CUT);
+		break;	
+	case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_ADVANCE_CUT:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_ADVANCE_CUT);
+		break;
+	case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_POISON_INJECTION:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_POISON_INJECTION);
+		break;
+	case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_POISON_STUN:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_POISON_STUN);
+		break;
+	case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_ASSASSINATION:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_ASSASSINATION);
+		break;
+	case en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_STEP:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_ASSASSINATION_ACTIVE_ATTACK_BACK_STEP);
+		break;
+	case en_SkillType::SKILL_ASSASSINATION_ACTIVE_BUF_STEALTH:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_ASSASSINATION_ACTIVE_BUF_STEALTH);
+		break;
+	case en_SkillType::SKILL_ASSASSINATION_ACTIVE_BUF_SIXTH_SENSE_MAXIMIZE:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_ASSASSINATION_ACTIVE_BUF_SIXTH_SENSE_MAXIMIZE);
+		break;
+	case en_SkillType::SKILL_SHOOTING_ACTIVE_ATTACK_SNIFING:
+		LearnSkillTypes.push_back(en_SkillType::SKILL_SHOOTING_ACTIVE_ATTACK_SNIFING);
+		break;		
 	}
+
+	for (en_SkillType LearnSkillType : LearnSkillTypes)
+	{
+		CSkill* ActiveSkill = FindSkill(LearnSkillType);
+		if (ActiveSkill != nullptr)
+		{
+			ActiveSkill->GetSkillInfo()->IsSkillLearn = IsSkillLearn;
+			ActiveSkill->GetSkillInfo()->SkillLevel = SkillLevel;
+		}
+	}	
 }
 
 CSkill* CSkillCharacteristic::FindSkill(en_SkillType FindSkillType)
